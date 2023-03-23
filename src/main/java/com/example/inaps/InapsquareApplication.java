23ea18fdd3f05e7cadda934f8e78639de5be15d3
SEONCANDO <seoncando@gmail.com>
2023-03-23 16:11:09 +0900

    first commit

---
+++ src/main/java/com/example/inaps/InapsquareApplication.java
@@ -0,0 +1,13 @@
+package com.example.inaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InapsquareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InapsquareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inaps/InapsquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InapsquareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
